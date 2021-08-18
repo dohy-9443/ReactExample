9c7b4e62df1e0c631aa8093973b7fed5bdc9604b
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 15:07:34 +0900

    undefined 렌더링 안하기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const App = () => {
   const name = '리웩트';
+  const unde = undefined;
   return (
     <>
       <h1>잘 작동하니</h1>
       <h2>어 잘 작동해</h2>
       <h3>나는 {name} 이야</h3>
       {name === '리액트' ? <h4>리액트입니다.</h4> : <h4>리액트가 아닙니다.</h4>}
+      <h5>{unde || '값이 undefined입니다.'}</h5>
     </>
   );
 };
