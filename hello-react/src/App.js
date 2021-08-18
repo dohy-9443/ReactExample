26834fe28d6b245165acad09273499cee0debe1d
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 19:01:44 +0900

    함수형에서 useState 사용하기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Counter from './Counter';
+import Say from './Say';
 
 const App = () => {
-  return <Counter />;
+  return (
+    <>
+      <Counter />
+      <br />
+      <Say />
+    </>
+  );
 };
 
 export default App;
