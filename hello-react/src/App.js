553c0ce62f02f3f01d873b53d86dc568749d3768
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 16:31:36 +0900

    주석

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 const App = () => {
   const name = '리액트';
-  return <div className="react">{name}</div>;
+  return (
+    <div className="react">
+      {name}
+      {/* 리액트 주석 */}
+      /* 이렇게 적으면 다 나옴 */ // 이것도 나옴
+    </div>
+  );
 };
 
 export default App;
