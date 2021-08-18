8845ccbcc2f093ff21b6f19338d21672d043424b
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 16:27:23 +0900

    예제6-2 선언 없이 바로 작성

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 const App = () => {
   const name = '리액트';
-  const style = {
-    backgroundColor: 'gray',
-    color: 'cyan',
-    fontSize: '2rem',
-    fontWight: 'bold',
-    padding: 16,
-  };
-  return <div style={style}>{name}</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: 'gray',
+        color: 'cyan',
+        fontSize: '2rem',
+        fontWight: 'bold',
+        padding: 16,
+      }}
+    >
+      {name}
+    </div>
+  );
 };
 
 export default App;
