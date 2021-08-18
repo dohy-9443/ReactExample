1a04af2c47d24f957f1cceab003263d6dedccdb5
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 16:45:20 +0900

    예제 클래스 문법

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,15 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-  const name = '리액트';
-  return (
-    <div className="react">
-      {name}
-      {/* 리액트 주석 */}
-      /* 이렇게 적으면 다 나옴 */ // 이것도 나옴
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    const name = 'react';
+    return <div className="react">{name}</div>;
+  }
+}
 
 export default App;
