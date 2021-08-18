79404ee5cf5328e985a83acfc37c9616d928838d
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 17:03:53 +0900

    태그 사이 내용을 보여 주는 children

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const MyComponent = (props) => {
-  return <div>내 이름은 {props.name} 입니다.</div>
+  return (
+    <div>
+      내 이름은 {props.name} 입니다.<br/>
+      children 값은 {props.children}입니다.
+    </div>
+  )
 };
 
 MyComponent.defaultProps = {
