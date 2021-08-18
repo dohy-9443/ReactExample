4e0877947e4900a12fa77e88f8d3aa4e4408d767
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 15:00:42 +0900

    예제4 prettier랑 변수

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const App = () => {
+  const name = '리액트';
   return (
     <>
       <h1>잘 작동하니</h1>
       <h2>어 잘 작동해</h2>
+      <h3>나는 {name} 이야</h3>
     </>
   );
 };
