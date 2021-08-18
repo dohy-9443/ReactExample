d9794b6e9584c59daef073152bc9cf25d74eba18
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 16:25:57 +0900

    예제 6 인라인 스타일링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
 const App = () => {
-  const name = '리웩트';
-  const unde = undefined;
-  return (
-    <>
-      <h1>잘 작동하니</h1>
-      <h2>어 잘 작동해</h2>
-      <h3>나는 {name} 이야</h3>
-      {name === '리액트' ? <h4>리액트입니다.</h4> : <h4>리액트가 아닙니다.</h4>}
-      <h5>{unde || '값이 undefined입니다.'}</h5>
-    </>
-  );
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'gray',
+    color: 'cyan',
+    fontSize: '2rem',
+    fontWight: 'bold',
+    padding: 16,
+  };
+  return <div style={style}>{name}</div>;
 };
 
 export default App;
