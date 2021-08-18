97b558e6209ce107737b0e4a7639d949675a39bd
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 18:54:44 +0900

    state 객체 안에 여러 값이 있을 때

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -6,20 +6,33 @@ class Counter extends Component {
     // state의 초깃값 설정하기
     this.state = {
       number: 0,
+      fixedNumber: 0,
     };
   }
 
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
+
     return (
       <div>
         <h1>{number}</h1>
+        <h2>{fixedNumber}: 바뀌지 않는 값</h2>
         <button
           onClick={() => {
             this.setState({ number: number + 1 });
           }}
+          disabled={number === 10 ? true : false}
         >
           +1
+        </button>
+
+        <button
+          onClick={() => {
+            this.setState({ number: number - 1 });
+          }}
+          disabled={number === -10 ? true : false}
+        >
+          -1
         </button>
       </div>
     );
