10c0661b39b0d84d6661647a3a08c7a42c601f1b
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-18 16:57:52 +0900

    props 기본값 설정: defaultProps

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MyComponent from './MyComponent';
 
 const App = () => {
-  return <MyComponent name="리액트" />;
+  return <MyComponent>리액트</MyComponent>;
 };
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const MyComponent = (props) => {
   return <div>내 이름은 {props.name} 입니다.</div>
-}
+};
+
+MyComponent.defaultProps = {
+  name: '기본 이름'
+};
 
 export default MyComponent;
