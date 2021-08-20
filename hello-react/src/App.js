fda2a3b43f406f94335289c1acc25caf54da8a1d
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-20 16:34:52 +0900

    class 끝

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -5,8 +5,23 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <>
@@ -26,6 +41,13 @@ class App extends Component {
         <br />
         <br />
         <IterationSample />
+        <br />
+        <br />
+        <div>
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <br />
+          <LifeCycleSample color={this.state.color} />
+        </div>
       </>
     );
   }
