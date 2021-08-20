97294f34bc75cef2776e9fa5f937c0f8c11ad272
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-20 14:02:37 +0900

    App 컴포넌트에 렌더링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,6 +4,7 @@ import Say from './Say';
 import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
@@ -22,6 +23,9 @@ class App extends Component {
         <button onClick={() => this.scrollBox.scrollToBottom()}>
           맨 밑으로
         </button>
+        <br />
+        <br />
+        <IterationSample />
       </>
     );
   }
