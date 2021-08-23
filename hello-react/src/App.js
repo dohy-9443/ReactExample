d4568a11d56c6e621f2f89b7520f503bf30aa248
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-23 15:18:06 +0900

    AND 연산자

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,12 +4,8 @@ const App = () => {
   const name = "리웩트";
   return (
     <div>
-      <h1>지금 name은 : {name}</h1>
-      {name === "리액트" ? (
-        <h1>리액트 입니다.</h1>
-      ) : (
-        <h2>리액트가 아닙니다.</h2>
-      )}
+      <h1>지금 name 값은 : {name}</h1>
+      <div>{name === "리액트" ? <h1>리액트입니다.</h1> : null}</div>
     </div>
   );
 };
