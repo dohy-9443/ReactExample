f95a089efef072ca792bf8410070f5f8b9dc52e4
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-23 19:14:19 +0900

    함수형 컴포넌트

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,81 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class EventPractice extends Component {
-  state = { username: '', message: '' };
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+const EventPractice = () => {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ': ' + message);
+    setUsername('');
+    setMessage('');
   };
-
-  handleClick = () => {
-    alert(this.state.username + ': ' + this.state.message);
-    this.setState({
-      username: '',
-      message: '',
-    });
-  };
-
-  handleKeyPress = (e) => {
+  const onKeyPress = (e) => {
     if (e.key === 'Enter') {
-      this.handleClick();
+      onClick();
     }
   };
 
-  render() {
-    return (
-      <div
+  return (
+    <div
+      style={{
+        border: '5px solid #CB9FFD',
+        background: '#BEEBFD',
+        padding: '2rem',
+        textAlign: 'center',
+      }}
+    >
+      <h1 style={{ color: 'white' }}>이벤트 연습</h1>
+      <h1 style={{ color: 'white' }}>{username + ': ' + message}</h1>
+      <br />
+      <input
         style={{
-          border: '5px solid #CB9FFD',
-          background: '#BEEBFD',
-          padding: '2rem',
-          textAlign: 'center',
+          padding: '1rem',
+          background: '#83a7a3',
+          color: '#BEEBFD',
+          fontSize: '20px',
+          fontWeight: 'bold',
         }}
-      >
-        <h1 style={{ color: 'white' }}>이벤트 연습</h1>
-        <h1 style={{ color: 'white' }}>
-          {this.state.username + ': ' + this.state.message}
-        </h1>
-        <br />
-        <input
-          style={{
-            padding: '1rem',
-            background: '#83a7a3',
-            color: '#BEEBFD',
-            fontSize: '20px',
-            fontWeight: 'bold',
-          }}
-          type="text"
-          name="username"
-          placeholder="사용자명을 입력하세요."
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        &nbsp;&nbsp;
-        <input
-          style={{
-            padding: '1rem',
-            background: '#83a7a3',
-            color: '#BEEBFD',
-            fontSize: '20px',
-            fontWeight: 'bold',
-          }}
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해 보세요."
-          value={this.state.message}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress}
-        />
-        <br />
-        <br />
-        <button style={{ padding: '1rem 2rem' }} onClick={this.handleClick}>
-          확인
-        </button>
-      </div>
-    );
-  }
-}
+        type="text"
+        name="username"
+        placeholder="사용자명을 입력하세요."
+        value={username}
+        onChange={onChangeUsername}
+      />
+      &nbsp;&nbsp;
+      <input
+        style={{
+          padding: '1rem',
+          background: '#83a7a3',
+          color: '#BEEBFD',
+          fontSize: '20px',
+          fontWeight: 'bold',
+        }}
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요."
+        value={message}
+        onChange={onChangeMessage}
+        onKeyPress={onKeyPress}
+      />
+      <br />
+      <br />
+      <button style={{ padding: '1rem 2rem' }} onClick={onClick}>
+        확인
+      </button>
+    </div>
+  );
+};
 
 export default EventPractice;
