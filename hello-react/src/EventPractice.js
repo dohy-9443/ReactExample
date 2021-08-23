5b399c157d28a21a0fe1a10ed9ac7c740254d944
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-23 18:58:29 +0900

    Property Initializer Syntax를 사용한 메서드 작성

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -3,24 +3,18 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
   state = { message: '' };
 
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
       message: e.target.value,
     });
-  }
+  };
 
-  handleClick() {
+  handleClick = () => {
     alert(this.state.message);
     this.setState({
       message: '',
     });
-  }
+  };
 
   render() {
     return (
