10f1aeb626de32118d34460d49612c806539044f
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-23 14:20:00 +0900

    Hello-react 다시 생성

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,56 +1,25 @@
-import React, { Component } from 'react';
-import Counter from './Counter';
-import Say from './Say';
-import EventPractice from './EventPractice';
-import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
-import IterationSample from './IterationSample';
-import LifeCycleSample from './LifeCycleSample';
+import logo from './logo.svg';
+import './App.css';
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-  state = {
-    color: '#000000',
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <Counter />
-        <br />
-        <Say />
-        <br />
-        <EventPractice />
-        <br />
-        <ValidationSample />
-        <br />
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <br />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-        <br />
-        <br />
-        <IterationSample />
-        <br />
-        <br />
-        <div>
-          <button onClick={this.handleClick}>랜덤 색상</button>
-          <br />
-          <LifeCycleSample color={this.state.color} />
-        </div>
-      </>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
