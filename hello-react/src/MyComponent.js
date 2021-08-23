ff0b7812ebdd5cf2123d3bb585162ebf7275a517
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-23 17:06:53 +0900

    클래스형 컴포넌트로 바꿈

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div
-      style={{
-        border: '5px solid aqua',
-        padding: '2rem',
-        background: 'black',
-        color: 'white',
-      }}
-    >
-      <div>나의 새롭고 멋진 컴포넌트</div>
-      <br />
-      <div>안녕하세요, 제 이름은 {name}입니다.</div>
-      <br />
-      <div>children 값은 {children}입니다.</div>
-      <br />
-      <div>제가 좋아하는 숫자는 {favoriteNumber}입니다.</div>
-    </div>
-  );
-};
+class MyComponent extends Component {
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+
+    return (
+      <div
+        style={{
+          border: '5px solid aqua',
+          padding: '2rem',
+          background: 'black',
+          color: 'white',
+        }}
+      >
+        <div>나의 새롭고 멋진 컴포넌트</div>
+        <br />
+        <div>안녕하세요, 제 이름은 {name}입니다.</div>
+        <br />
+        <div>children 값은 {children}입니다.</div>
+        <br />
+        <div>제가 좋아하는 숫자는 {favoriteNumber}입니다.</div>
+      </div>
+    );
+  }
+}
 
 MyComponent.defaultProps = {
   name: '기본 이름',
