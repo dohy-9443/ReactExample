296167b555920d812c1aaa258b1e2426e816ed35
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-23 14:45:09 +0900

    If 문 대신 조건부 연산자

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const App = () => {
-  const name = "리액트";
+  const name = "리웩트";
   return (
-    <>
-      <h1>{name} 안녕!</h1>
-      <h2>잘 작동하니?</h2>
-    </>
+    <div>
+      <h1>지금 name은 : {name}</h1>
+      {name === "리액트" ? (
+        <h1>리액트 입니다.</h1>
+      ) : (
+        <h2>리액트가 아닙니다.</h2>
+      )}
+    </div>
   );
 };
 
