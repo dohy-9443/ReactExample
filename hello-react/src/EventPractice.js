11787dd515846d9b89b4ec6459550aa1fe8f0f8c
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-23 18:25:31 +0900

    컴포넌트 생성

---
+++ hello-react/src/EventPractice.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class EventPractice extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          border: '5px solid #CB9FFD',
+          background: '#BEEBFD',
+          padding: '2rem',
+          textAlign: 'center',
+        }}
+      >
+        <h1 style={{ color: 'white' }}>이벤트 연습</h1>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
