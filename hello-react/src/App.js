ac992864695bdb143f0486f0ea4f8107f50e2e60
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-23 15:20:21 +0900

    undefined를 렌덜이하지 않기 근데 JSX 내부에서 undefined를 렌더링하는 것은 괜찮

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 
 const App = () => {
-  const name = "리웩트";
-  return (
-    <div>
-      <h1>지금 name 값은 : {name}</h1>
-      <div>{name === "리액트" ? <h1>리액트입니다.</h1> : null}</div>
-    </div>
-  );
+  const name = undefined;
+  return name || "값이 undefined입니다.";
 };
 
 export default App;
