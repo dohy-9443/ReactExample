2450dab79f4d9e00baa38765cc68ae5adfd6666a
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-23 18:32:40 +0900

    onChange 이벤트 설정

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -12,6 +12,22 @@ class EventPractice extends Component {
         }}
       >
         <h1 style={{ color: 'white' }}>이벤트 연습</h1>
+        <br />
+        <input
+          style={{
+            padding: '1rem',
+            background: '#83a7a3',
+            color: '#BEEBFD',
+            fontSize: '20px',
+            fontWeight: 'bold',
+          }}
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요."
+          onChange={(e) => {
+            console.log(e);
+          }}
+        />
       </div>
     );
   }
