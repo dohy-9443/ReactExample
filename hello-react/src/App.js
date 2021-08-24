565468368b6138a94096918977e015a3bf8ba0a2
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-24 16:53:09 +0900

    클래스형으로 변경

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
 import Say from './Say';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 
-const App = () => {
-  return (
-    <div>
-      <MyComponent name="React" favoriteNumber={3}>
-        리액트
-      </MyComponent>
-      <br />
-      <Counter />
-      <br />
-      <Say />
-      <br />
-      <EventPractice />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MyComponent name="React" favoriteNumber={3}>
+          리액트
+        </MyComponent>
+        <br />
+        <Counter />
+        <br />
+        <Say />
+        <br />
+        <EventPractice />
+        <br />
+        <ValidationSample />
+      </div>
+    );
+  }
+}
 
 export default App;
