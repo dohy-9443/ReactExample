ae17e94e68542e4a2c7a24109cc3da13f41c5833
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-26 18:55:08 +0900

    데이터 배열을 컴포넌트 배열로 변환

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 const IterationSample = () => {
+  const names = ['눈사람', '얼음', '눈', '바람'];
+  const nameList = names.map((name) => (
+    <li style={{ color: 'white', fontSize: '1rem' }}>{name}</li>
+  ));
   return (
-    <div>
-      <ul>
-        <li>눈사람</li>
-        <li>얼음</li>
-        <li>눈</li>
-        <li>바람</li>
-      </ul>
+    <div
+      style={{
+        border: '5px solid #ecc7co',
+        background: '#fdae84',
+        padding: '2rem',
+      }}
+    >
+      <ul style={{ textAlign: 'center' }}>{nameList}</ul>
     </div>
   );
 };
