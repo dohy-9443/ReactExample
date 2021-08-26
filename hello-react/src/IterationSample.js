7a8118a993c0cc1426409db81b29b3effc840d01
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-26 23:46:01 +0900

    항목 추가 까지 작성

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText('');
+  };
+
   const nameList = names.map((name, index) => (
     <li key={index} style={{ color: 'white', fontSize: '1rem' }}>
-      {name}
+      {name.text}
     </li>
   ));
   return (
@@ -15,6 +34,9 @@ const IterationSample = () => {
         padding: '2rem',
       }}
     >
+      <input value={inputText} onChange={onChange} />
+      &nbsp;&nbsp;
+      <button onClick={onClick}>추가</button>
       <ul style={{ textAlign: 'center' }}>{nameList}</ul>
     </div>
   );
