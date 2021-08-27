3c493036bc969ffe74922dfc7f0599d99409f8ba
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-27 21:15:23 +0900

    렌더링

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Counter from "./Counter";
+import Info from "./Info";
 
 const App = () => {
   return (
     <div>
       <Counter />
+      <br />
+      <Info />
     </div>
   );
 };
