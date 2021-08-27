0ee6e4817a4bcb9cafc63fc9da0857c8b16811b8
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-27 17:18:36 +0900

    에러바인더리 감쌈

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,6 +7,7 @@ import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
 import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -61,7 +62,9 @@ class App extends Component {
           }}
         >
           <button onClick={this.handleClick}>랜덤 색상</button>
-          <LifeCycleSample color={this.state.color} />
+          <ErrorBoundary>
+            <LifeCycleSample color={this.state.color} />
+          </ErrorBoundary>
         </div>
       </div>
     );
