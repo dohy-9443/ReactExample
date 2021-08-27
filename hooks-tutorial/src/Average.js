b2d4b08a82ceda82906e3b085aa42207485873f4
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-27 22:16:27 +0900

    useMemo 평균값

---
+++ hooks-tutorial/src/Average.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const getAverage = (numbers) => {
+  console.log("평균값 계산 중...");
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
+};
+
+const Average = () => {
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState("");
+
+  const onChange = (e) => {
+    setNumber(e.target.value);
+  };
+  const onInsert = (e) => {
+    const nextList = list.concat(parseInt(number));
+    setList(nextList);
+    setNumber("");
+  };
+
+  return (
+    <div
+      style={{
+        border: "5px solid #909fa6",
+        background: "#d1dfe8",
+        padding: "2rem",
+        textAlign: "center",
+      }}
+    >
+      <input value={number} onChange={onChange} />
+      &nbsp;
+      <button onClick={onInsert}>등록</button>
+      <br />
+      <ul>
+        {list.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <br />
+      <div>
+        <b>평균값 : </b> {getAverage(list)}
+      </div>
+    </div>
+  );
+};
+
+export default Average;
