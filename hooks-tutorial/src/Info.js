dffb2a7920b6152adf715c608f1f0b22deace81d
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-27 21:31:37 +0900

    뒷정리하기

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -5,7 +5,12 @@ const Info = () => {
   const [nickname, setNickname] = useState("");
 
   useEffect(() => {
-    console.log("특정 값이 업데이트될 때만 실행", name);
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
   }, [name]);
 
   const onChangeName = (e) => {
