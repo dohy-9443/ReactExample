a378a0aa377440c3eda0e3e725f02506baaf915f
dohy-9443 <dong_hyun95@hanmail.net>
2021-08-27 21:42:31 +0900

    가시성 해보기

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./Counter";
 import Info from "./Info";
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div>
       <Counter />
       <br />
-      <Info />
+
+      <div style={{ textAlign: "center" }}>
+        <button
+          onClick={() => {
+            setVisible(!visible);
+          }}
+        >
+          {visible ? "숨기기" : "보이기"}
+        </button>
+        <br />
+        <br />
+        {visible && <Info />}
+      </div>
     </div>
   );
 };
