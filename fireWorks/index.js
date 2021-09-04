3186795d53c592b88fc5e8f66845fee8152f30b9
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-04 19:04:28 +0900

    진행중2

--- fireWorks/index.js
+++ fireWorks/index.js
@@ -10,17 +10,55 @@ const RandomColor = () => {
 container.addEventListener("click", (e) => {
   const fire = document.createElement("div");
   fire.id = "fire-box";
+
+  let color = RandomColor();
+
   const key = keyCount;
   keyCount += 1;
   i += 1;
 
-  let color = RandomColor();
+  const childFire1 = document.createElement("div");
+  childFire1.classList.add("fire1");
+  const childFire2 = document.createElement("div");
+  childFire2.classList.add("fire2");
+  const childFire3 = document.createElement("div");
+  childFire3.classList.add("fire3");
+  const childFire4 = document.createElement("div");
+  childFire4.classList.add("fire4");
+  const childFire5 = document.createElement("div");
+  childFire5.classList.add("fire5");
+  const childFire6 = document.createElement("div");
+  childFire6.classList.add("fire6");
+  const childFire7 = document.createElement("div");
+  childFire7.classList.add("fire7");
+  const childFire8 = document.createElement("div");
+  childFire8.classList.add("fire8");
 
   fire.setAttribute("data-key", key);
   container.appendChild(fire);
+
+  fire.appendChild(childFire1);
+  fire.appendChild(childFire2);
+  fire.appendChild(childFire3);
+  fire.appendChild(childFire4);
+  fire.appendChild(childFire5);
+  fire.appendChild(childFire6);
+  fire.appendChild(childFire7);
+  fire.appendChild(childFire8);
+
   fire.style.left = e.offsetX + "px";
   fire.style.top = e.offsetY + "px";
   fire.style.background = color;
+  fire.style.zIndex = i;
+
+  childFire1.style.background = color;
+  childFire2.style.background = color;
+  childFire3.style.background = color;
+  childFire4.style.background = color;
+  childFire5.style.background = color;
+  childFire6.style.background = color;
+  childFire7.style.background = color;
+  childFire8.style.background = color;
 
   // console.log(fire.style.left, fire.style.top);
   fire.classList.add("display");
