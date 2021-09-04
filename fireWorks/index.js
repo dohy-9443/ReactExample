f31037b135a93f59ccea74cebdd77abf9c15043f
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-04 17:56:30 +0900

    진행중1

--- fireWorks/index.js
+++ fireWorks/index.js
@@ -1,12 +1,18 @@
 const container = document.querySelector("#container");
-const fire = document.querySelector("#fire-box");
 
-const boom = (e) => {
+let keyCount = 0;
+
+container.addEventListener("click", (e) => {
+  const fire = document.createElement("div");
+  fire.id = "fire-box";
+  const key = keyCount;
+  keyCount += 1;
+
+  fire.setAttribute("data-key", key);
+  container.appendChild(fire);
   fire.style.left = e.offsetX + "px";
   fire.style.top = e.offsetY + "px";
 
   // console.log(fire.style.left, fire.style.top);
   fire.classList.add("display");
-};
-
-container.addEventListener("click", boom);
+});
