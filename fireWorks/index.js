c3ebbdf14c59961278c4806177c25ccc27129f15
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-04 18:29:45 +0900

    자동으로 엘리먼트 사라지게 작성

--- fireWorks/index.js
+++ fireWorks/index.js
@@ -1,18 +1,34 @@
 const container = document.querySelector("#container");
 
 let keyCount = 0;
+let i = 0;
+
+const RandomColor = () => {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+};
 
 container.addEventListener("click", (e) => {
   const fire = document.createElement("div");
   fire.id = "fire-box";
   const key = keyCount;
   keyCount += 1;
+  i += 1;
+
+  let color = RandomColor();
 
   fire.setAttribute("data-key", key);
   container.appendChild(fire);
   fire.style.left = e.offsetX + "px";
   fire.style.top = e.offsetY + "px";
+  fire.style.background = color;
 
   // console.log(fire.style.left, fire.style.top);
   fire.classList.add("display");
+
+  if (i === keyCount) {
+    setTimeout(() => {
+      container.removeChild(fire);
+      console.log(i, keyCount);
+    }, 5000);
+  }
 });
